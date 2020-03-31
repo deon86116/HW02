c1e7af34c1ee609fd4aefbba2e2df4b726fba81f
deon86116 <deon86116@gmail.com>
2020-04-01 00:23:27 +0800

    ss

--- HW02/main.cpp
+++ HW02/main.cpp
@@ -11,28 +11,28 @@ DigitalOut redLED(LED2);
 
 
 int i;
-float count_ = 0;
+float freqcnt = 0;
 int sample = 600;
-float ADCdata[600];
+float ADCinput[600];
 float freq = 0;
-// float ADCdata;
+
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 char dot[10] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF};
 int main(){
     for(i = 0; i < sample; i++) {
         Aout = Ain;
-        ADCdata[i] = Ain;
+        ADCinput[i] = Ain;
         wait(1./sample);
     }
     for(i = 0; i < sample; i++) {
         if(i != 0) {
-            if(ADCdata[i-1] < 0.3 && ADCdata[i] >= 0.3) count_ = count_ + 1; 
+            if(ADCinput[i-1] < 0.3 && ADCinput[i] >= 0.3) freqcnt = freqcnt + 1; 
         }
-        pc.printf("%1.3f\r\n", ADCdata[i]);
+        pc.printf("%1.3f\r\n", ADCinput[i]);
     }
-    freq = round(count_);
-    int tmp = (int) freq;
+    freq = round(freqcnt);
+    int digit = (int) freq;
     float k;
     int x = 0;
     if((int)freq >= 100) x = 100;
@@ -40,14 +40,14 @@ int main(){
     while(1) {
         if(Switch == 0) {
             if(x == 1) {
-                display = dot[tmp];
-                tmp = (int) freq;
+                display = dot[digit];
+                digit = (int) freq;
                 if((int)freq >= 100) x = 100;
                 else x = 10;
             }
             else {
-                display = table[tmp/x];
-                tmp %= x;
+                display = table[digit/x];
+                digit %= x;
                 x /= 10;
             }
             wait(1);
